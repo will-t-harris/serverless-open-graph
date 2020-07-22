d9da7c8d55aa6935000a3e783a73b9a26d73911b
Will Harris <will.t.harris@protonmail.com>
2020-07-21 22:57:18 -0700

    define playwright screenshot handler

--- functions/gen-og-image/gen-og-image.js
+++ functions/gen-og-image/gen-og-image.js
@@ -1,6 +1,36 @@
-exports.handler = async function (event, context) {
+const playwright = require("playwright-aws-lambda");
+
+exports.handler = async function (event, ctx) {
+	const browser = await playwright.launchChromium();
+	const context = await browser._defaultContext;
+	const page = await context.newPage();
+	await page.setContent(`
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="utf-8" />
+		</head>
+
+		<body>
+			<div id="test">hello</div>
+		</body>
+	</html>
+	`);
+	const boundingRect = await page.evaluate(() => {
+		const test = document.getElementById("test");
+		const { x, y, width, height } = test.children[0].getBoundingClientRect();
+		return { x, y, width, height };
+	});
+
+	const screenshotBuffer = await page.screenshot({ clip: boundingRect });
+	await browser.close();
 	return {
+		isBase64Encoded: true,
 		statusCode: 200,
-		body: "gen function working",
+		headers: {
+			"Content-Type": "image/png",
+			"Content-Length": screenshotBuffer.length.toString(),
+		},
+		body: screenshotBuffer.toString("base64"),
 	};
 };
